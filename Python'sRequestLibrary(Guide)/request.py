64d96c6d2695aa17a18120cba5d428b847725e0d
goshinh <jiller@126.com>
2020-04-25 20:18:03 +0800

    202004252017

--- Python'sRequestLibrary(Guide)/request.py
+++ Python'sRequestLibrary(Guide)/request.py
@@ -2,10 +2,9 @@ import requests
 
 source = requests.get('https://api.github.com')
 
-print(source.status_code)
+# print(source.status_code)
 
 if source.status_code == 200:
     print("Success!")
 elif source.status_code == 404:
-    print('Not Found.')
-    
+    print('Not Found.')   
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # LearningPython
 This repo is about the src I wrote in Python.
-plus!
